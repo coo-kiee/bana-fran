4b56b0b7bb036728ce62afbf3419eb92569591bb
kys <yskang@banaple.co.kr>
2022-11-22 12:31:16 +0900

    :bug: MembershipService 관련 수정

--- src/service/membershipService.ts
+++ src/service/membershipService.ts
@@ -1,11 +1,14 @@
-import { useQuery } from 'react-query'
+import { useMutation, useQuery } from 'react-query'
 import { queryFn } from 'hooks/useQuery'
 import { AxiosError } from 'axios';
 
 // type
 import { RequestParams } from 'types/common';
-import { EtcTotalParams, EtcListParams, TotalResultType } from 'types/etc/etcType';
-import { MembershipTotalType } from 'types/membership/extraType';
+import { EtcTotalParams, EtcListParams } from 'types/etc/etcType';
+import { MembershipListType, MembershipTotalType } from 'types/membership/extraType';
+import { RankEditParams } from 'types/membership/monthRankType';
+import Utils from 'utils/Utils';
+import { format } from 'date-fns';
 
 // TODO: 직전월 스탬프/쿠폰/바나포인트 내역 
 const useMembershipTotal = (params: EtcTotalParams) => {
@@ -14,10 +17,11 @@ const useMembershipTotal = (params: EtcTotalParams) => {
         query: 'MASFCWHJIICQKQGQFWMM',
         params: params,
     }; // web_fran_s_membership_total
-    return useQuery<MembershipTotalType, AxiosError>(['membership_total', params.fran_store], () => queryFn.getData(reqData), {
+    return useQuery<MembershipTotalType, AxiosError>(['membership_extra_total', params.fran_store], () => queryFn.getData(reqData), {
         keepPreviousData: false,
         refetchOnWindowFocus: false,
         retry: false,
+        suspense: true,
         onError: (err: any) => {
             queryFn.axiosError(err);
         }
@@ -31,13 +35,30 @@ const useMembershipList = (params: EtcListParams) => {
         query: 'KFVDLH7FKG9QKBVTK8TL',
         params: params,
     }; // web_fran_s_membership_list
-    return useQuery(['membership_list', params.from_date, params.to_date], () => queryFn.getDataList(reqData), {
+    return useQuery<MembershipListType[], AxiosError>(['membership_extra_list', params.fran_store, params.from_date, params.to_date], () => queryFn.getDataList(reqData), {
         keepPreviousData: false,
         refetchOnWindowFocus: false,
         retry: false,
-        refetchOnMount: true,
+        // suspense: true,
         onError: (err: any) => {
             queryFn.axiosError(err);
+        },
+        select: (data: MembershipListType[]) => {
+            const tempData = data.map((el, idx) => {
+                let tempData: any = {}
+                Object.entries(el).forEach((e) => {
+                    const key = e[0] as keyof MembershipListType;
+                    const value = e[1];
+                    if (key === 'std_date') {
+                        if (idx === 0) tempData['std_date'] = '합계';
+                        else tempData['std_date'] = format(new Date(value), 'yyyy/MM/dd');
+                    } // 날짜 처리
+                    else tempData[key] = Utils.numberComma(value); // 단위 처리
+                });
+                return tempData;
+            });
+            // console.log(tempData === data) // false
+            return tempData;
         }
     });
 };
@@ -48,15 +69,31 @@ const useRankInfo = (params: EtcTotalParams) => {
         keepPreviousData: false,
         refetchOnWindowFocus: false,
         retry: false,
+        suspense: true,
         onError: (err: any) => {
             queryFn.axiosError(err);
         }
     });
 }; // web_fran_s_membership_rank_info
 
-// const useRankEdit = () => {
+const useRankEdit = (params: RankEditParams) => {
+    // 한번에 여러개 가져와서 map으로?
+    const reqData: RequestParams<RankEditParams> = { ws: 'fprocess', query: 'HXKIEJM6QUTPFMM6LK5V', params };
 
-// }; // web_fran_u_membership_rank_info
+    return useMutation({
+        mutationFn: () => queryFn.getData(reqData)
+    })
+}; // web_fran_u_membership_rank_info
+
+const useRankEditList = (data: RankEditParams[]) => {
+    const mutateRank1 = useRankEdit(data[0]);
+    const mutateRank2 = useRankEdit(data[1]);
+    const mutateRank3 = useRankEdit(data[2]);
+    const mutateRank4 = useRankEdit(data[3]);
+    const mutateRank5 = useRankEdit(data[4]);
+
+    return { mutateRank1, mutateRank2, mutateRank3, mutateRank4, mutateRank5 }
+};
 
 const useRankList = (params: EtcListParams) => {
     const reqData: RequestParams<EtcListParams> = { ws: 'fprocess', query: 'KUUM9RON9HGD55IBRLQB', params };
@@ -65,6 +102,7 @@ const useRankList = (params: EtcListParams) => {
         keepPreviousData: false,
         refetchOnWindowFocus: false,
         retry: false,
+        suspense: true,
         onError: (err: any) => {
             queryFn.axiosError(err);
         }
@@ -75,7 +113,8 @@ const MEMBERSHIP_SERVICE = {
     useMembershipTotal,
     useMembershipList,
     useRankInfo,
-    useRankList
+    useRankList,
+    useRankEditList
 };
 
 export default MEMBERSHIP_SERVICE;
