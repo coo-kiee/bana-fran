56360ca365c44a956a9be71a0bf1f32fbc70b822
khwan <khchoi@banaple.co.kr>
2022-11-22 14:24:02 +0900

    🎨 sticky header 구조 수정(StickyContainer 통합)

--- src/pages/common/sticky/index.tsx
+++ src/pages/common/sticky/index.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from 'react';
-// Components
-import StickyContainer from './StickyContainer';
+import styled from 'styled-components';
 
 interface StickyProps {
     reference: HTMLTableRowElement | null; // tableRow target ref (sticky 적용 기준이 될 tr 가리키는 ref.current)
-    children: React.ReactNode; // children: colgroup, th. table의 colgroup과 sticky(fixed)될 tr, th 요소들
+    children: React.ReactNode; // children: colgroup, th. (table의 colgroup과 sticky(fixed)될 tr, th 요소들)
 };
 
 const Sticky = ({ reference, children }: StickyProps) => {
@@ -13,10 +12,9 @@ const Sticky = ({ reference, children }: StickyProps) => {
 
 	// change sticky states
     const handleSticky = (entries: any, observer: any) => {
-        // console.log(observer)
         entries.forEach((entry: any) => {
             if (!entry.isIntersecting) setShowSticky((prev) => !prev);
-            entry.intersectionRatio <= 0.1 ? setShowSticky(true) : setShowSticky(false);
+            entry.intersectionRatio <= 0.1 ? setShowSticky(true) : setShowSticky(false); // 10퍼센트 이하로 보이면 sticky 작동
         });
     }
 
@@ -28,8 +26,19 @@ const Sticky = ({ reference, children }: StickyProps) => {
 	}, [reference]);
 
 	return <>
-        { showSticky ? <StickyContainer>{children}</StickyContainer> : null }
+        { showSticky ? <StickyContainer className='board-wrap'>{children}</StickyContainer> : null }
     </>;
 };
 
 export default Sticky;
+
+const StickyContainer = styled.table`    
+    position: fixed;
+    top: 0;
+	z-index: 10;
+    border-radius: 0;
+    margin-top: 0 !important;
+    width: calc(100% - 290px);
+	border-spacing: 0;
+	padding: 0;
+`
