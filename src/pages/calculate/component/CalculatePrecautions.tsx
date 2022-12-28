0a638f815d04f248b59bed05371d618b943d3897
장민우 <mwjang@banaple.co.kr>
2022-12-28 12:09:38 +0900

    💄 UI 수정요청사항 반영

--- src/pages/calculate/component/CalculatePrecautions.tsx
+++ src/pages/calculate/component/CalculatePrecautions.tsx
@@ -22,7 +22,7 @@ export default CalculatePrecautions;
 
 // Component Type
 const PRECAUTIONS_INFO = {
-    [CALCULATE_TYPE.LIST]: ['※ 직전 월 정산내역을 확인 후 [정산확인]을 완료하시면 본사에서 세금계산서를 발행합니다.', '※ 당월 5일까지 직전월의 정산확인을 하지 않을 시 자동으로 정산확인 완료 처리됩니다.'],
+    [CALCULATE_TYPE.LIST]: ['※ 직전 월 정산내역을 확인 후 [정산확인]을 완료하시면 본사에서 세금계산서를 발행합니다.'],
     [CALCULATE_TYPE.POINT]: ['※ 유상포인트(충전포인트/잔돈포인트)의 결제내역을 조회 가능 합니다. (무상으로 제공되는 바나포인트는 제외).'],
     [CALCULATE_TYPE.COUPON]: ['※ 본사에서 비용을 부담하는 쿠폰의 결제내역을 조회 가능 합니다. (가맹점 비용 부담인 월간랭킹쿠폰, 스탬프적립쿠폰 등은 제외)'],
     [CALCULATE_TYPE.CLAIM]: ['※ 해당 가맹점의 과실로 인한 고객 클레임을 보상하기 위해 본사에서 임의 발행한 ‘서비스 쿠폰’에 대한 청구 내역을 조회할 수 있습니다.'],
