941bb6c4a532ba0a4cbce93ff1aee2372d931e8f
장민우 <mwjang@banaple.co.kr>
2023-11-07 10:54:07 +0900

    🎨 공통 - Tab 타입 오류 수정

--- src/pages/common/tab/index.tsx
+++ src/pages/common/tab/index.tsx
@@ -1,18 +1,27 @@
 import { useState } from 'react';
 
+// Util
+import Utils from 'utils/Utils';
+
 interface ITab<T> {
   tabTitleObj: T;
   render: (tabType: keyof T) => JSX.Element;
 }
 
 const Tab = <T extends Record<string | number, string>>({ tabTitleObj, render }: ITab<T>) => {
-  const [tabType, setTabType] = useState(Object.keys(tabTitleObj)[0]);
+  const [tabType, setTabType] = useState(
+    Utils.isNumber(tabTitleObj[0]) ? Number(Object.keys(tabTitleObj)[0]) : Object.keys(tabTitleObj)[0],
+  );
 
   return (
     <>
       <ul className="tab-wrap">
         {Object.entries(tabTitleObj).map(([type, title], index) => (
-          <li key={index} className={`tab ${type === tabType ? 'active' : ''}`} onClick={() => setTabType(type)}>
+          <li
+            key={index}
+            className={`tab ${type === String(tabType) ? 'active' : ''}`}
+            onClick={() => setTabType(typeof tabType === 'number' ? Number(type) : type)}
+          >
             {title}
           </li>
         ))}
