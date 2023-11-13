874c3173464f6180b02eeb6d246281d063f8731b
장민우 <mwjang@banaple.co.kr>
2023-11-13 09:25:19 +0900

    🐛 페이지 개수 선택 적용함수 수정

--- src/pages/common/pagination/SelectListRow.tsx
+++ src/pages/common/pagination/SelectListRow.tsx
@@ -1,17 +1,21 @@
 import { useState } from 'react';
 
 // Hook
-import useOnChange from 'hooks/useOnChange';
 import usePageInfo from 'hooks/pagination/usePageInfo';
 
 const SelectListRow = () => {
   const [rows] = useState([20, 30, 50, 100]);
 
-  const { pageInfo, setPageInfo } = usePageInfo();
-  const handlePageInfo = useOnChange(setPageInfo);
+  const { pageInfo, handleRow } = usePageInfo();
 
   return (
-    <select className="filter-number" name="row" id="" onChange={handlePageInfo} value={pageInfo.row}>
+    <select
+      className="filter-number"
+      name="row"
+      id=""
+      onChange={(e) => handleRow(Number(e.currentTarget.value))}
+      value={pageInfo.row}
+    >
       {rows.map((item) => (
         <option key={item} value={item}>
           {item}개
