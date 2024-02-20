b4232bf7288eb03d16251fdb097037837531fbb8
kys <yskang@banaple.co.kr>
2024-02-20 13:53:20 +0900

    🎨 공통 select aria-label 기본값 작성

--- src/pages/common/select/index.tsx
+++ src/pages/common/select/index.tsx
@@ -2,7 +2,6 @@ import { ChangeEventHandler } from 'react';
 
 interface ISelect {
   className?: string;
-  label?: string;
   name: string;
   value: string | number;
   options: readonly {
@@ -11,13 +10,17 @@ interface ISelect {
   }[];
   handleOnChange: ChangeEventHandler;
 }
-const Select = ({ className, name, value, options, label = '', handleOnChange }: ISelect) => {
+const Select = ({ className, name, value, options, handleOnChange }: ISelect) => {
   return (
     <select
       className={className}
       name={name}
       value={value}
-      aria-label={label || Object.values(options)[0].label.replace(' 전체', '')}
+      aria-label={
+        Object.values(options)[0].label.includes(' 전체')
+          ? Object.values(options)[0].label.replace(' 전체', '')
+          : '구분'
+      }
       onChange={handleOnChange}
     >
       {Object.values(options).map((item) => (
