505536d900ba4d56756a72c39287d4a6d31eced5
khwan <khchoi@banaple.co.kr>
2024-03-25 10:17:28 +0900

    💄 주문내역 - 강조 태그 변경

--- src/pages/sales/history/index.tsx
+++ src/pages/sales/history/index.tsx
@@ -143,7 +143,7 @@ const SalesHistory = () => {
               </p>
               <p className="hyphen notification">
                 본사쿠폰(미보전): 본사 발행 이벤트/프로모션 쿠폰 중 가맹점 부담 쿠폰. (23/5/1일부터{' '}
-                <span className="emphasis">1,500원 앱전용 쿠폰</span> 가맹점 부담)
+                <em className="emphasis">1,500원 앱전용 쿠폰</em> 가맹점 부담)
               </p>
             </div>
             <div className="board-filter">
